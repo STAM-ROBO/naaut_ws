39a8c1d70c35f013c7e1bb01c4c0ceb6a744349a
Davide Di Gloria <d.digloria@stamtech.com>
2025-04-17 17:31:16 +0200

    added simple simulator

---
+++ src/simulator/naaut_sim/naaut_sim/run_sim.py
@@ -0,0 +1,174 @@
+import rclpy
+from rclpy.node import Node
+import numpy as np
+from geometry_msgs.msg import Twist
+import time
+import threading
+from sensor_msgs.msg import NavSatFix
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Imu
+import math
+from tf2_ros import TransformBroadcaster
+from geometry_msgs.msg import TransformStamped
+from tf_transformations import quaternion_from_euler, euler_from_quaternion
+
+class naaut_simulator(Node):
+    def __init__(self):
+        super().__init__('naaut_simulator')       
+        
+        self.declare_parameter('orientation_topic','/imu_gnss')
+        self.declare_parameter('imu_link','imu_link')
+        self.declare_parameter('fix_topic','/gps/fix')
+        self.declare_parameter('gnss_link','gps_link')
+        self.declare_parameter('odometry_topic','/odometry/gps')
+        self.orientation_topic = self.get_parameter('orientation_topic').get_parameter_value().string_value
+        self.imu_link = self.get_parameter('imu_link').get_parameter_value().string_value
+        self.fix_topic = self.get_parameter('fix_topic').get_parameter_value().string_value
+        self.gnss_link = self.get_parameter('gnss_link').get_parameter_value().string_value
+        self.odometry_topic = self.get_parameter('odometry_topic').get_parameter_value().string_value
+        
+        #sim params
+        self.time_step = 0.1
+        self.mass = 150.0
+
+        # 1/2 * water_density * area_in_water * Cd
+        self.drag_coefficient = 0.5 * 1000 * (3.14 * 0.26**2) * 0.2
+        
+        #rectangular vessel
+        self.rot_drag=0.005
+        self.vessel_inertia = (1 / 12) * self.mass * (2**2 + 2**2)
+        
+        self.x_vel=0.0
+        self.rz_vel=0.0     
+        
+        self.roll_freq=2.0   
+        self.roll_amp=np.radians(10)
+        
+        self.orientation_pub  = self.create_publisher(Imu, self.orientation_topic,  10)
+        self.fix_pub        = self.create_publisher(NavSatFix, self.fix_topic,  10)
+        self.cmd_vel_sub = self.create_subscription(Twist,'cmd_vel', self.get_cmd_vel, 10)
+        self.odometry_topic_pub  = self.create_publisher(Odometry,  self.odometry_topic,  10)                
+        self.tf_broadcaster = TransformBroadcaster(self)
+        
+        run_sim_loop_thread = threading.Thread(target=self.run_sim_loop, daemon=True)
+        run_sim_loop_thread.start()
+    
+    def get_cmd_vel(self,msg):
+        self.x_vel=msg.linear.x
+        self.rz_vel=msg.angular.z  
+               
+    def run_sim_loop(self):
+        self.get_logger().info("Starting simu loop")
+        lin_vel = np.array([0.0, 0.0])
+        lin_pos = np.array([0.0, 0.0])
+        heading_dot= 0.0
+        heading = 0.0     
+        pitch=0.0
+        roll= 0.0
+        timestamp=0.0
+                       
+        while rclpy.ok():  
+            torque = self.rz_vel  
+            thrust = self.x_vel     
+            angular_acceleration = torque / self.vessel_inertia
+            heading_dot += angular_acceleration * self.time_step - heading_dot*self.rot_drag
+            heading += heading_dot * self.time_step
+            thrust_vect= np.array([np.cos(heading), np.sin(heading)])*thrust
+            
+            if np.linalg.norm(lin_vel) > 0:
+                drag_force = self.drag_coefficient * np.linalg.norm(lin_vel) * lin_vel
+            else:
+                drag_force = np.array([0.0, 0.0])
+
+            net_force = thrust_vect - drag_force
+            acceleration = net_force / self.mass
+            lin_vel += acceleration * self.time_step
+            lin_pos += lin_vel * self.time_step
+            
+            roll = self.roll_amp*math.sin(2 * math.pi * self.roll_freq * timestamp)
+            self.pub_simu_results(heading, pitch, roll, lin_pos, lin_vel)
+            timestamp+=self.time_step
+            
+            time.sleep(self.time_step)           
+        self.get_logger().info("Finished sim")
+    
+    def pub_simu_results(self, heading, pitch, roll,  xy_location, xy_velocity):          
+        this_time = self.get_clock().now().to_msg()
+        
+        #Publish GPS Fix Data
+        #fix_msg = NavSatFix()
+        #fix_msg.header.stamp = this_time
+        #fix_msg.header.frame_id = self.gnss_link
+        #fix_msg.latitude = bestpos_lat
+        #fix_msg.longitude = bestpos_lon
+        #fix_msg.altitude = bestpos_hgt
+        #fix_msg.position_covariance[0] = float(bestpos_latstd)**2
+        #fix_msg.position_covariance[4] = float(bestpos_lonstd)**2
+        #fix_msg.position_covariance[8] = float(bestpos_hgtstd)**2
+        #fix_msg.position_covariance_type = NavSatFix.COVARIANCE_TYPE_DIAGONAL_KNOWN
+        
+        #Publish emulated IMU Data
+        imu_message = Imu()
+        imu_message.header.frame_id = self.imu_link
+        imu_message.header.stamp = self.get_clock().now().to_msg()    
+        quaternion = quaternion_from_euler(math.radians(0), math.radians(0), math.radians(heading))
+        imu_message.orientation.x = quaternion[0]
+        imu_message.orientation.y = quaternion[1]
+        imu_message.orientation.z = quaternion[2]
+        imu_message.orientation.w = quaternion[3]        
+        imu_message.orientation_covariance    = [0.0] * 9
+            
+        #publish odometry with gps and UTM
+        odom_msg = Odometry()
+        odom_msg.header.stamp = this_time
+        odom_msg.header.frame_id = 'map'
+        odom_msg.child_frame_id  = 'gps_link'
+        odom_msg.pose.pose.position.x = xy_location[0]
+        odom_msg.pose.pose.position.y = xy_location[1]
+        odom_msg.pose.pose.position.z = 0.0
+        quaternion = quaternion_from_euler(math.radians(roll), math.radians(pitch), math.radians(heading))
+        odom_msg.pose.pose.orientation.x = quaternion[0]
+        odom_msg.pose.pose.orientation.y = quaternion[1]
+        odom_msg.pose.pose.orientation.z = quaternion[2]
+        odom_msg.pose.pose.orientation.w = quaternion[3]
+        odom_msg.pose.covariance         = [0.0] * 36
+        odom_msg.pose.covariance[21]     = 0.1
+        odom_msg.pose.covariance[28]     = 0.1
+        odom_msg.pose.covariance[35]     = 0.1
+        odom_msg.twist.twist.linear.x    = xy_velocity[0]
+        odom_msg.twist.twist.linear.y    = xy_velocity[1]
+        odom_msg.twist.twist.linear.z    = 0.0
+        odom_msg.twist.covariance        = [0.0] * 36
+       
+        #publish all messages
+        self.odometry_topic_pub.publish(odom_msg)
+        self.publish_transform_from_odometry(odom_msg)
+    
+    def publish_transform_from_odometry(self, odom_msg):
+        t = TransformStamped()
+        t.header.frame_id = "odom"
+        t.child_frame_id = "base_link"
+        t.transform.translation.x = odom_msg.pose.pose.position.x
+        t.transform.translation.y = odom_msg.pose.pose.position.y
+        t.transform.translation.z = 0.0
+        t.transform.rotation.x = odom_msg.pose.pose.orientation.x
+        t.transform.rotation.y = odom_msg.pose.pose.orientation.y
+        t.transform.rotation.z = odom_msg.pose.pose.orientation.z
+        t.transform.rotation.w = odom_msg.pose.pose.orientation.w
+        self.tf_broadcaster.sendTransform(t)    
+
+    def shutdown(self):
+        self.get_logger().info("Shutting down node.")
+        rclpy.shutdown()
+
+def main(args=None):
+    rclpy.init(args=args)
+    node=naaut_simulator()
+    
+    try:
+        rclpy.spin(node)  # Keep node running to handle callbacks
+    except KeyboardInterrupt:
+        node.get_logger().info("KeyboardInterrupt, shutting down.")
+
+if __name__ == '__main__':
+    main()
